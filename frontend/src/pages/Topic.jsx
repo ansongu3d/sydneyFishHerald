a505baa53afea406cfa2efb9f7b43c16cdda5ed5
ag3d <sydneybettafish@gmail.com>
2022-10-27 22:34:17 +1100

    8th

--- frontend/src/pages/Topic.jsx
+++ frontend/src/pages/Topic.jsx
@@ -1,47 +1,47 @@
-import { useEffect, useState } from 'react'
-import { toast } from 'react-toastify'
-import Modal from 'react-modal'
-import { FaPlus } from 'react-icons/fa'
-import { useSelector, useDispatch } from 'react-redux'
-import { getTopic, closeTopic } from '../features/topics/topicSlice'
-import { getNotes, createNote } from '../features/notes/noteSlice'
-import { useParams, useNavigate } from 'react-router-dom'
-import BackButton from '../components/BackButton'
-import Spinner from '../components/Spinner'
-import NoteItem from '../components/NoteItem'
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import Modal from "react-modal";
+import { FaPlus } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
+import { getTopic, closeTopic } from "../features/topics/topicSlice";
+import { getNotes, createNote } from "../features/notes/noteSlice";
+import { useParams, useNavigate } from "react-router-dom";
+import BackButton from "../components/BackButton";
+import Spinner from "../components/Spinner";
+import NoteItem from "../components/NoteItem";
 
 const customStyles = {
   content: {
-    width: '600px',
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
-    position: 'relative',
+    width: "600px",
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    position: "relative",
   },
-}
+};
 
-Modal.setAppElement('#root')
+Modal.setAppElement("#root");
 
 function Topic() {
-  const [modalIsOpen, setModalIsOpen] = useState(false)
-  const [noteText, setNoteText] = useState('')
-  const { topic } = useSelector((state) => state.topics)
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [noteText, setNoteText] = useState("");
+  const { topic } = useSelector((state) => state.topics);
 
-  const { notes } = useSelector((state) => state.notes)
+  const { notes } = useSelector((state) => state.notes);
 
   // NOTE: no need for two useParams
   // const params = useParams()
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
-  const { topicId } = useParams()
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { topicId } = useParams();
 
   useEffect(() => {
-    dispatch(getTopic(topicId)).unwrap().catch(toast.error)
-    dispatch(getNotes(topicId)).unwrap().catch(toast.error)
-  }, [topicId, dispatch])
+    dispatch(getTopic(topicId)).unwrap().catch(toast.error);
+    dispatch(getNotes(topicId)).unwrap().catch(toast.error);
+  }, [topicId, dispatch]);
 
   // Close topic
   const onTopicClose = () => {
@@ -50,37 +50,37 @@ function Topic() {
     dispatch(closeTopic(topicId))
       .unwrap()
       .then(() => {
-        toast.success('Topic Closed')
-        navigate('/topics')
+        toast.success("Topic Closed");
+        navigate("/topics");
       })
-      .catch(toast.error)
-  }
+      .catch(toast.error);
+  };
 
   // Create note submit
   const onNoteSubmit = (e) => {
     // NOTE: we can unwrap our AsyncThunkACtion here so no need for isError and
     // isSuccess state
-    e.preventDefault()
+    e.preventDefault();
     dispatch(createNote({ noteText, topicId }))
       .unwrap()
       .then(() => {
-        setNoteText('')
-        closeModal()
+        setNoteText("");
+        closeModal();
       })
-      .catch(toast.error)
-  }
+      .catch(toast.error);
+  };
 
   // Open/close modal
-  const openModal = () => setModalIsOpen(true)
-  const closeModal = () => setModalIsOpen(false)
+  const openModal = () => setModalIsOpen(true);
+  const closeModal = () => setModalIsOpen(false);
 
   if (!topic) {
-    return <Spinner />
+    return <Spinner />;
   }
 
   return (
-    <div className='topic-page'>
-      <header className='topic-header'>
+    <div className="topic-page">
+      <header className="topic-header">
         <BackButton />
         <h2>
           Topic ID: {topic._id}
@@ -89,21 +89,35 @@ function Topic() {
           </span>
         </h2>
         <h3>
-          Date Submitted: {new Date(topic.createdAt).toLocaleString('en-US')}
+          Date Submitted: {new Date(topic.createdAt).toLocaleString("en-US")}
         </h3>
         <h3>Catogory: {topic.catogory}</h3>
         <hr />
-        <div className='topic-desc'>
-          <h3>Description of catch</h3>
+        <div className="topic-desc">
+          <h3>Description of Fishing</h3>
+          <p>
+            <strong>Fishing Gear: </strong>
+            {topic.fishingGear}
+          </p>
+
+          <p>
+            <strong>Fish Size: </strong>
+            {topic.fishSize} (mm)
+          </p>
+
+          <p>
+            <strong>Fishing Spot: </strong>
+            {topic.location}
+          </p>
+          
+          <img className="fish-img" src={topic.fishImage} />
           <p>{topic.description}</p>
-          <p><strong>Fish Size: </strong>{topic.fishSize} (mm)</p>
-          <img className='fish-img' src={topic.fishImage}/>
         </div>
         <h2>Notes</h2>
       </header>
 
-      {topic.status !== 'closed' && (
-        <button onClick={openModal} className='btn'>
+      {topic.status !== "closed" && (
+        <button onClick={openModal} className="btn">
           <FaPlus /> Add Note
         </button>
       )}
@@ -112,25 +126,25 @@ function Topic() {
         isOpen={modalIsOpen}
         onRequestClose={closeModal}
         style={customStyles}
-        contentLabel='Add Note'
+        contentLabel="Add Note"
       >
         <h2>Add Note</h2>
-        <button className='btn-close' onClick={closeModal}>
+        <button className="btn-close" onClick={closeModal}>
           X
         </button>
         <form onSubmit={onNoteSubmit}>
-          <div className='form-group'>
+          <div className="form-group">
             <textarea
-              name='noteText'
-              id='noteText'
-              className='form-control'
-              placeholder='Note text'
+              name="noteText"
+              id="noteText"
+              className="form-control"
+              placeholder="Note text"
               value={noteText}
               onChange={(e) => setNoteText(e.target.value)}
             ></textarea>
           </div>
-          <div className='form-group'>
-            <button className='btn' type='submit'>
+          <div className="form-group">
+            <button className="btn" type="submit">
               Submit
             </button>
           </div>
@@ -143,13 +157,13 @@ function Topic() {
         <Spinner />
       )}
 
-      {topic.status !== 'closed' && (
-        <button onClick={onTopicClose} className='btn btn-block btn-danger'>
+      {topic.status !== "closed" && (
+        <button onClick={onTopicClose} className="btn btn-block btn-danger">
           Close Topic
         </button>
       )}
     </div>
-  )
+  );
 }
 
-export default Topic
+export default Topic;
